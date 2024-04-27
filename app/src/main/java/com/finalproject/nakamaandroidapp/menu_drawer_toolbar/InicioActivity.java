b447496cc62a533b47f07d3107de82b5af33a13a
KimiVarela <constanzavarelasanz@gmail.com>
2024-04-27 11:30:20 +0200

    Added, menu, drawer, home and config layout

---
+++ app/src/main/java/com/finalproject/nakamaandroidapp/menu_drawer_toolbar/InicioActivity.java
@@ -0,0 +1,41 @@
+package com.finalproject.nakamaandroidapp.menu_drawer_toolbar;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.finalproject.nakamaandroidapp.R;
+import com.finalproject.nakamaandroidapp.databinding.ActivityInicioBinding;
+import com.google.android.material.slider.Slider;
+
+public class InicioActivity extends DrawerBaseActivity {
+    ActivityInicioBinding activityInicioBinding;
+    Button boton1;
+    TextView label1;
+    TextView label2;
+    TextView label3;
+    TextView label4;
+    Spinner spinner1;
+    Spinner spinner2;
+    Spinner spinner3;
+    Slider slider1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityInicioBinding= activityInicioBinding.inflate(getLayoutInflater());
+
+        setContentView(activityInicioBinding.getRoot());
+
+        boton1 = (Button) findViewById(R.id.inicio_boton);
+        label1 = (TextView) findViewById(R.id.inicio_label1);
+        label2 = (TextView) findViewById(R.id.inicio_label2);
+        label3 = (TextView) findViewById(R.id.inicio_label3);
+        label4 = (TextView) findViewById(R.id.inicio_label4);
+        spinner1 = (Spinner) findViewById(R.id.inicio_spinner1);
+        spinner2 = (Spinner) findViewById(R.id.inicio_spinner2);
+        spinner3 = (Spinner) findViewById(R.id.spinner_rango);
+        slider1 = (Slider) findViewById(R.id.inicio_slider);
+    }
+}
