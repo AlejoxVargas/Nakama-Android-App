b4d74510e9c329469d88db919671051487fe6972
AlejoxVargas <henrryalejandro21@gmail.com>
2024-05-04 17:48:06 +0200

    Added createData in CreateProfileActivity.java to collect the data from the user and send it to Firestore

--- app/src/main/java/com/finalproject/nakamaandroidapp/CreateProfileActivity.java
+++ app/src/main/java/com/finalproject/nakamaandroidapp/CreateProfileActivity.java
@@ -1,22 +1,41 @@
 package com.finalproject.nakamaandroidapp;
 
+import static android.content.ContentValues.TAG;
+
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CreateProfileActivity extends AppCompatActivity {
 
     Spinner spinnerGames;
     Spinner spinnerRegion;
+    TextView tvCategory;
+    Button btnCreate;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +43,46 @@ public class CreateProfileActivity extends AppCompatActivity {
 
         spinnerGames = (Spinner) findViewById(R.id.spinner_Juegos);
         spinnerRegion = (Spinner) findViewById(R.id.spinner_Region);
+        tvCategory = (TextView) findViewById(R.id.input_rango);
+        btnCreate = (Button) findViewById(R.id.boton_empezar);
 
         ArrayAdapter<CharSequence> adapterRegion= ArrayAdapter.createFromResource(this, R.array.region, android.R.layout.simple_spinner_item);
         ArrayAdapter<CharSequence> adapterGames= ArrayAdapter.createFromResource(this, R.array.games, android.R.layout.simple_spinner_item);
 
         spinnerRegion.setAdapter(adapterRegion);
         spinnerGames.setAdapter(adapterGames);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String region = spinnerRegion.getSelectedItem().toString();
+                String favoriteGame = spinnerGames.getSelectedItem().toString();
+                String category = tvCategory.getText().toString();
+                createData(region, favoriteGame, category);
+            }
+        });
+    }
+
+    public void createData(String region, String favoriteGame, String category) {
+        Map<String, Object> subscription = new HashMap<>();
+        subscription.put("region", region);
+        subscription.put("favorite_game", favoriteGame);
+        subscription.put("category", category);
+
+        db.collection("profiles")
+                .add(subscription).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        Intent intent = new Intent(CreateProfileActivity.this, homePrueba.class);
+                        startActivity(intent);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                        Toast.makeText(CreateProfileActivity.this, "error at created ", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
